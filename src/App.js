4e24130c94a57b197828fd96360e1c57fa602b75
Nischay Pandey <nischaypandey11@gmail.com>
2020-02-18 11:00:57 +0530

    added react routers

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Main from './components/MainComponent';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
 
 class App extends Component{
   
 
 render() {
   return (
-    <div >
-      
+    <BrowserRouter>
+          <div >
+
+            
       <Main />
-    </div>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 }
